c6df20af46e2a98c45e52ce3da553b6e6d46a90f
Itamar Haimov <ihaimov@acad.jct.ac.il>
2025-05-18 18:14:18 +0300

    average

--- PopularityBasedRecommender.java
+++ PopularityBasedRecommender.java
@@ -14,12 +14,18 @@ class PopularityBasedRecommender<T extends Item> extends RecommenderSystem<T> {
     @Override
     public List<T> recommendTop10(int userId) {
         // TODO: implement
+
         return null;
     }
 
     public double getItemAverageRating(int itemId) {
-        // TODO: implement
-        return 0;
+
+        return ratings.stream()
+                .filter(r -> r.getItemId()==itemId)
+                .mapToDouble(Rating::getRating)
+                .average()
+                .orElse(0.0);
+
     }
     public int getItemRatingsCount(int itemId) {
         // TODO: implement
